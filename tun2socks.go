7b13131f9b1c4041aee59bf23a49fca99101badd
xxf098 <ufxmeng@gmail.com>
2020-01-04 05:22:55 -0500

    create vmess json config

--- tun2socks.go
+++ tun2socks.go
@@ -2,6 +2,7 @@ package tun2socks
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -16,6 +17,7 @@ import (
 	vcore "v2ray.com/core"
 	vproxyman "v2ray.com/core/app/proxyman"
 	vbytespool "v2ray.com/core/common/bytespool"
+	verrors "v2ray.com/core/common/errors"
 	vnet "v2ray.com/core/common/net"
 	v2filesystem "v2ray.com/core/common/platform/filesystem"
 	v2serial "v2ray.com/core/infra/conf/serial"
@@ -35,9 +37,122 @@ const (
 	v2Assert = "v2ray.location.asset"
 )
 
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *verrors.Error {
+	return verrors.New(values...).WithPathObj(errPathObjHolder{})
+}
+
+type vmess struct {
+	Host     string
+	Path     string
+	TLS      string
+	Add      string
+	Port     int
+	Aid      int
+	Net      string
+	ID       string
+	Loglevel string
+}
+
 // type DBService interface {
 // 	InsertProxyLog(target, tag string, startTime, endTime int64, uploadBytes, downloadBytes int32, recordType, dnsQueryType int32, dnsRequest, dnsResponse string, dnsNumIPs int32)
 // }
+
+func generateVmessConfig(profile vmess) ([]byte, error) {
+	vmessConfig := v2ray.VmessConfig{}
+	vmessConfig.Log = v2ray.Log{Access: "", Error: "", Loglevel: profile.Loglevel}
+	vmessConfig.DNS = v2ray.DNS{
+		Servers: []string{"1.0.0.1", "localhost"},
+		Hosts:   v2ray.Hosts{"baidu.com": "127.0.0.1", "umeng.com": "127.0.0.1"},
+	}
+	vmessConfig.Routing = v2ray.Routing{
+		DomainStrategy: "IPIfNonMatch",
+		Rules: []v2ray.Rules{
+			v2ray.Rules{
+				Type:        "field",
+				OutboundTag: "direct",
+				IP:          []string{"geoip:private", "geoip:cn"},
+			},
+			v2ray.Rules{
+				Type:        "field",
+				OutboundTag: "direct",
+				Domain:      []string{"geosite:cn"},
+			},
+		},
+	}
+	vmessConfig.Inbounds = []v2ray.Inbounds{
+		v2ray.Inbounds{
+			Tag:      "socks-in",
+			Protocol: "socks",
+			Port:     8088,
+			Listen:   "::",
+			InboundsSettings: &v2ray.InboundsSettings{
+				Auth: "noauth",
+				IP:   "127.0.0.1",
+				UDP:  true,
+			},
+		},
+		v2ray.Inbounds{
+			Tag:      "http-in",
+			Protocol: "http",
+			Port:     8090,
+			Listen:   "::",
+		},
+	}
+	outbound := v2ray.Outbounds{
+		Tag:      "proxy",
+		Protocol: "vmess",
+		Mux:      &v2ray.Mux{Enabled: true},
+		Settings: v2ray.OutboundsSettings{
+			Vnext: []v2ray.Vnext{
+				v2ray.Vnext{
+					Address: profile.Add,
+					Port:    profile.Port,
+					Users: []v2ray.Users{
+						v2ray.Users{
+							AlterID:  profile.Aid,
+							Email:    "v2ray@email.com",
+							ID:       profile.ID,
+							Security: "auto",
+						},
+					},
+				},
+			},
+		},
+		StreamSettings: &v2ray.StreamSettings{},
+	}
+	if profile.Net == "ws" {
+		outbound.StreamSettings = &v2ray.StreamSettings{
+			Network: profile.Net,
+			Wssettings: v2ray.Wssettings{
+				ConnectionReuse: true, Path: profile.Path,
+			},
+		}
+		if profile.Host != "" {
+			outbound.StreamSettings.Wssettings.Headers = v2ray.Headers{
+				Host: profile.Host,
+			}
+		}
+	}
+	if profile.TLS == "tls" {
+		outbound.StreamSettings.Security = profile.TLS
+		outbound.StreamSettings.TLSSettings = &v2ray.TLSSettings{AllowInsecure: true}
+	}
+	// vmess must be the first
+	vmessConfig.Outbounds = []v2ray.Outbounds{
+		outbound,
+		v2ray.Outbounds{
+			Protocol: "freedom",
+			Tag:      "direct",
+			Settings: v2ray.OutboundsSettings{
+				DomainStrategy: "UseIP",
+			},
+		},
+	}
+	// errStr, _ := json.Marshal(vmessConfig)
+	return json.MarshalIndent(vmessConfig, "", "    ")
+}
 
 // VpnService should be implemented in Java/Kotlin.
 type VpnService interface {
@@ -77,7 +192,7 @@ func SetLocalDNS(dns string) {
 
 // StartV2Ray sets up lwIP stack, starts a V2Ray instance and registers the instance as the
 // connection handler for tun2socks.
-func StartV2Ray(packetFlow PacketFlow, vpnService VpnService, configBytes []byte, assetPath, proxyLogDBPath string) error {
+func StartV2Ray(packetFlow PacketFlow, vpnService VpnService, configBytes []byte, assetPath string, proxyLogDBPath string) error {
 	if packetFlow != nil {
 		// if dbService != nil {
 		// 	vsession.DefaultDBService = dbService
@@ -143,6 +258,134 @@ func StartV2Ray(packetFlow PacketFlow, vpnService VpnService, configBytes []byte
 	return errors.New("packetFlow is null")
 }
 
+func GenerateVmessString(
+	host string,
+	path string,
+	tls string,
+	add string,
+	port int,
+	aid int,
+	net string,
+	id string,
+	loglevel string,
+) (string, error) {
+	profile := vmess{
+		Host:     host,
+		Path:     path,
+		TLS:      tls,
+		Add:      add,
+		Port:     port,
+		Aid:      aid,
+		Net:      net,
+		ID:       id,
+		Loglevel: loglevel,
+	}
+	configBytes, err := generateVmessConfig(profile)
+	if err != nil {
+		log.Fatal("start V instance failed: %v", err)
+		return "", err
+	}
+	return string(configBytes), nil
+}
+
+// StartV2Ray sets up lwIP stack, starts a V2Ray instance and registers the instance as the
+// connection handler for tun2socks.
+func StartV2RayWithVmess(
+	packetFlow PacketFlow,
+	vpnService VpnService,
+	host string,
+	path string,
+	tls string,
+	add string,
+	port int,
+	aid int,
+	net string,
+	id string,
+	loglevel string,
+	assetPath string) error {
+	if packetFlow != nil {
+		// if dbService != nil {
+		// 	vsession.DefaultDBService = dbService
+		// }
+
+		if lwipStack == nil {
+			// Setup the lwIP stack.
+			lwipStack = core.NewLWIPStack()
+		}
+
+		// Assets
+		os.Setenv("v2ray.location.asset", assetPath)
+
+		// Protect file descriptors of net connections in the VPN process to prevent infinite loop.
+		protectFd := func(s VpnService, fd int) error {
+			if s.Protect(fd) {
+				return nil
+			} else {
+				return errors.New(fmt.Sprintf("failed to protect fd %v", fd))
+			}
+		}
+		netCtlr := func(network, address string, fd uintptr) error {
+			return protectFd(vpnService, int(fd))
+		}
+		vinternet.RegisterDialerController(netCtlr)
+		vinternet.RegisterListenerController(netCtlr)
+
+		// Share the buffer pool.
+		core.SetBufferPool(vbytespool.GetPool(core.BufSize))
+
+		// Start the V2Ray instance.
+		// v, err = vcore.StartInstance("json", configBytes)
+		profile := vmess{
+			Host:     host,
+			Path:     path,
+			TLS:      tls,
+			Add:      add,
+			Port:     port,
+			Aid:      aid,
+			Net:      net,
+			ID:       id,
+			Loglevel: loglevel,
+		}
+		configBytes, err := generateVmessConfig(profile)
+		if err != nil {
+			log.Fatal("start V instance failed: %v", err)
+			return err
+		}
+		v, err = vcore.StartInstance("json", configBytes)
+		if err != nil {
+			log.Fatal("start V instance failed: %v", err)
+			return err
+		}
+
+		// Configure sniffing settings for traffic coming from tun2socks.
+		sniffingConfig := &vproxyman.SniffingConfig{
+			Enabled:             true,
+			DestinationOverride: strings.Split("tls,http", ","),
+		}
+		ctx := vproxyman.ContextWithSniffingConfig(context.Background(), sniffingConfig)
+
+		// Register tun2socks connection handlers.
+		// vhandler := v2ray.NewHandler(ctx, v)
+		// core.RegisterTCPConnectionHandler(vhandler)
+		// core.RegisterUDPConnectionHandler(vhandler)
+		core.RegisterTCPConnHandler(v2ray.NewTCPHandler(ctx, v))
+		core.RegisterUDPConnHandler(v2ray.NewUDPHandler(ctx, v, 2*time.Minute))
+
+		// Write IP packets back to TUN.
+		core.RegisterOutputFn(func(data []byte) (int, error) {
+			if !isStopped {
+				packetFlow.WritePacket(data)
+			}
+			return len(data), nil
+		})
+
+		isStopped = false
+		return nil
+	}
+	return errors.New("packetFlow is null")
+}
+
+// StopV2Ray stop v2ray
 func StopV2Ray() {
 	isStopped = true
 	if lwipStack != nil {
@@ -192,6 +435,7 @@ func initV2Env(assetperfix string) {
 
 func TestConfig(ConfigureFileContent string, assetperfix string) error {
 	initV2Env(assetperfix)
+	// os.Setenv("v2ray.location.asset", assetperfix)
 	_, err := v2serial.LoadJSONConfig(strings.NewReader(ConfigureFileContent))
 	return err
 }
