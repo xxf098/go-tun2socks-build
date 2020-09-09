d6394e3528375ebec299853e11775dda20b01cf6
xxf098 <ufxmeng@gmail.com>
2020-09-08 21:02:08 -0400

    telegram ip ranges

--- common.go
+++ common.go
@@ -420,7 +420,18 @@ func createRouterConfig(routeMode int) *conf.RouterConfig {
 	gfwListIP, _ := json.Marshal(v2ray.Rules{
 		Type:        "field",
 		OutboundTag: "proxy",
-		IP:          []string{"1.1.1.1/32", "1.0.0.1/32", "8.8.8.8/32", "8.8.4.4/32"},
+		IP: []string{
+			"1.1.1.1/32",
+			"1.0.0.1/32",
+			"8.8.8.8/32",
+			"8.8.4.4/32",
+			"149.154.160.0/22",
+			"149.154.164.0/22",
+			"91.108.4.0/22",
+			"91.108.56.0/22",
+			"91.108.8.0/22",
+			"95.161.64.0/20",
+		},
 	})
 	chinaListSite, _ := json.Marshal(v2ray.Rules{
 		Type:        "field",
