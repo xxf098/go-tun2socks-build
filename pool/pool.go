7602096a068a3dcbf99256c49cb6b991156fe3d7
xxf098 <ufxmeng@gmail.com>
2020-09-11 22:19:54 -0400

    bytes pool

---
+++ pool/pool.go
@@ -0,0 +1,35 @@
+package pool
+
+import (
+	"sync"
+)
+
+var pool *sync.Pool
+
+const BufSize = 32 * 1024
+
+func NewBytes(size int) []byte {
+	if size <= BufSize {
+		return pool.Get().([]byte)
+	} else {
+		return make([]byte, size)
+	}
+}
+
+func FreeBytes(b []byte) {
+	b = b[0:cap(b)] // restore slice
+	if cap(b) >= BufSize {
+		pool.Put(b)
+	}
+}
+
+func init() {
+	pool = &sync.Pool{
+		New: func() interface{} {
+			// The Pool's New function should generally only return pointer
+			// types, since a pointer can be put into the return interface
+			// value without an allocation:
+			return make([]byte, BufSize)
+		},
+	}
+}
