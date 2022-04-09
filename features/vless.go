60d374e2330479cfd18c73565887eb7223465760
xxf098 <ufxmeng@gmail.com>
2022-04-09 11:18:41 -0400

    add NewVless

--- features/vless.go
+++ features/vless.go
@@ -11,3 +11,18 @@ type Vless struct {
 	Protocol string
 	VmessOptions
 }
+
+func NewVless(Add string, Port int, Password string, TLS string, HeaderType string, Security string, ID string, Net string, opt []byte) *Vless {
+	options := NewVmessOptions(opt)
+	return &Vless{
+		TLS:          TLS,
+		Add:          Add,
+		Port:         Port,
+		Net:          Net,
+		ID:           ID,
+		Type:         HeaderType,
+		Security:     Security,
+		Protocol:     "vless",
+		VmessOptions: options,
+	}
+}
